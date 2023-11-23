7be3653e9e53d3009cb547fe002d5dbfc7fad3f4
Dmitrii <MIntriy2033@yandex.ru>
2023-11-23 21:27:55 +0100

    maze class intepretation

---
+++ src/main/kotlin/com/app/astar/model/Maze.kt
@@ -0,0 +1,112 @@
+package com.app.astar.model
+
+import java.io.File
+import java.lang.Exception
+import java.lang.RuntimeException
+
+enum class Signs(val char: Char) {
+    WALL('#'),
+    UNVISITED('.'),
+    EXIT('E'),
+    START('S'),
+    PATH('*');
+
+    override fun toString() = char.toString();
+}
+class Maze private constructor() {
+    private var mazeList: MutableList<MutableList<Signs>> = mutableListOf()
+    private var goalPos: Point = Point(-1, -1)
+    private var startPos: Point = Point(-1, -1)
+    private var colCount: Int = -1
+    private var rowCount: Int = -1
+    companion object {
+        fun fromFile(pathToFile: String) : Maze {
+            val maze = Maze()
+            try {
+                val lines = File(pathToFile).readLines()
+                maze.rowCount = lines.size
+                lines.forEachIndexed { rowIndex, line ->
+                    val row = mutableListOf<Signs>()
+                    line.forEach { char ->
+                        when (char) {
+                            Signs.UNVISITED.char -> row.add(Signs.UNVISITED)
+                            Signs.WALL.char -> row.add(Signs.WALL)
+                            Signs.START.char -> {
+                                row.add(Signs.START)
+                                maze.startPos = Point(rowIndex, row.lastIndex)
+                            }
+                            Signs.EXIT.char -> {
+                                row.add(Signs.EXIT)
+                                maze.goalPos = Point(rowIndex, row.lastIndex)
+                            }
+                            ' ' -> {} // skip space
+                            else -> throw RuntimeException("Unknown character in the maze: $char")
+                        }
+                    }
+                    maze.mazeList.add(row)
+                }
+                maze.colCount = maze.mazeList[0].size
+            } catch (e: Exception) {
+                throw RuntimeException("Error reading file: $pathToFile, ${e.message}")
+            }
+
+            if (maze.startPos.col == -1 || maze.goalPos.col == -1){
+                throw RuntimeException("The maze doesn't have start or exit position")
+            }
+            return maze
+        }
+        fun copyOf(other: Maze): Maze {
+            val newMaze = Maze()
+            newMaze.goalPos = other.goalPos
+            newMaze.startPos = other.startPos
+            newMaze.colCount = other.colCount
+            newMaze.rowCount = other.rowCount
+            newMaze.mazeList = other.mazeList.map { it.toMutableList() }.toMutableList()
+            return newMaze
+        }
+    }
+
+    fun outOfRange(index: Point) : Boolean {
+        return index.col < 0 || index.col >= rowCount || index.row < 0 || index.row >= colCount
+    }
+
+    operator fun get(point: Point): Signs {
+        if (outOfRange(point)) {
+            throw IndexOutOfBoundsException("Index out of bounds: $point")
+        }
+        return mazeList[point.col][point.row]
+    }
+
+    operator fun set(point: Point, sign: Signs) {
+        if (outOfRange(point)) {
+            throw IndexOutOfBoundsException("Index out of bounds: $point")
+        }
+        mazeList[point.col][point.row] = sign
+    }
+
+    fun isWall(point: Point) : Boolean{
+        return this[point] == Signs.WALL
+    }
+
+    fun printBySteps(path: ArrayList<Point>){
+        val tempMaze = this
+        for (point in path){
+            if (point != startPos && point != goalPos){
+                tempMaze[point] = Signs.PATH
+            }
+            tempMaze.print()
+        }
+    }
+
+    fun print() {
+        for (row in mazeList){
+            for (cell in row){
+                print("$cell ")
+            }
+            println();
+        }
+        println()
+    }
+
+}
+
---
+++ src/main/kotlin/com/app/astar/model/Point.kt
@@ -0,0 +1,33 @@
+package com.app.astar.model
+
+import kotlin.math.abs
+
+class Point (val col: Int, val row: Int) : Comparable<Point> {
+    fun getDistOfPoints(other: Point): Int {
+        return abs(col - other.col) + abs(row - other.row)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Point) return false
+
+        return col == other.col && row == other.row
+    }
+
+    override fun compareTo(other: Point): Int {
+        if (this.col == other.col && this.row == other.row) {
+            return 0
+        }
+        return when {
+            col > other.col -> 1
+            col < other.col -> -1
+            else -> row.compareTo(other.row)
+        }
+    }
+
+    override fun toString(): String {
+        return "($col, $row)"
+    }
+
+
+}

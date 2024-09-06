2063753255ac161354d7b70732b2404b09bf3145
Dmitrii Popov <MIntriy2033@yandex.ru>
2024-09-06 13:15:53 +0200

    GridController refactoring

--- src/main/kotlin/com/app/astar/controller/GridController.kt
+++ src/main/kotlin/com/app/astar/controller/GridController.kt
@@ -8,7 +8,7 @@ import javafx.scene.input.MouseButton
 import javafx.scene.paint.Color
 import javafx.scene.layout.*
 
-class GridController (private var rows: Int = 0, private var cols: Int = 0) {
+class GridController(private var rows: Int = 0, private var cols: Int = 0) {
 
     val grid = GridPane()
 
@@ -22,23 +22,11 @@ class GridController (private var rows: Int = 0, private var cols: Int = 0) {
     }
 
     constructor(maze: Maze) : this(maze.colCount, maze.rowCount) {
-        for (rowIndex in maze.mazeList.indices) {
-            for (colIndex in maze.mazeList[rowIndex].indices) {
-                val sign = maze.mazeList[rowIndex][colIndex]
-                val paneColor = getPaneColor(sign)
-                val pane = createPane(paneColor)
-                if (paneColor == ColorSigns.START.color){
-                    startPane = pane
-                } else if (paneColor == ColorSigns.EXIT.color) {
-                    endPane = pane
-                }
-
-                grid.add(pane, colIndex, rowIndex)
-            }
-        }
+        initializeGridFromMaze(maze)
     }
 
-    private fun getPaneColor(sign: Signs) : Color {
+    // Метод для получения цвета панели на основе знака
+    private fun getPaneColor(sign: Signs): Color {
         return when (sign) {
             Signs.EXIT -> ColorSigns.EXIT.color
             Signs.UNVISITED -> ColorSigns.UNVISITED.color
@@ -49,110 +37,96 @@ class GridController (private var rows: Int = 0, private var cols: Int = 0) {
         }
     }
 
-
-    /*
-    function for clearing PATH and PROBABLE
-     */
-    fun clearPath(){
-        for (node in grid.children){
+    // Объединенный метод для очистки панелей
+    private fun clearNodes(condition: (Color) -> Boolean) {
+        for (node in grid.children) {
             if (node is Pane) {
-                val backgroundColor = node.background?.fills?.firstOrNull()?.fill
-                if (backgroundColor == ColorSigns.PATH.color || backgroundColor == ColorSigns.PROBABLE.color){
-                    val newBackgroundFill = BackgroundFill(ColorSigns.UNVISITED.color, CornerRadii.EMPTY, Insets.EMPTY)
-                    val newBackground = Background(newBackgroundFill)
-                    node.background = newBackground
+                val backgroundColor = (node.background?.fills?.firstOrNull()?.fill as? Color) // Приведение к Color
+                if (backgroundColor != null && condition(backgroundColor)) {
+                    updatePaneColor(node, ColorSigns.UNVISITED.color)
                 }
             }
         }
+    }
 
+    // Очистка только путей и вероятных путей
+    fun clearPath() {
+        clearNodes { it == ColorSigns.PATH.color || it == ColorSigns.PROBABLE.color }
         onPathClearedCallback?.invoke()
     }
 
-    fun clearAll(){
-        for (node in grid.children){
-            if (node is Pane) {
-                val backgroundColor = node.background?.fills?.firstOrNull()?.fill
-                if (backgroundColor != ColorSigns.UNVISITED.color){
-                    // Создаем новый объект BackgroundFill с новым цветом
-                    val newBackgroundFill = BackgroundFill(ColorSigns.UNVISITED.color, CornerRadii.EMPTY, Insets.EMPTY)
-                    // Создаем новый объект Background с новым BackgroundFill
-                    val newBackground = Background(newBackgroundFill)
-                    // Устанавливаем новый фон для Pane
-                    node.background = newBackground
-                }
-            }
-        }
+    // Полная очистка сетки
+    fun clearAll() {
+        clearNodes { it != ColorSigns.UNVISITED.color }
         startPane = null
         endPane = null
     }
 
+    // Метод для создания ячеек с обработчиками кликов
     private fun createPane(paneBackgroundColor: Color): Pane {
         return Pane().apply {
             background = Background(BackgroundFill(paneBackgroundColor, null, null))
             setPrefSize(20.0, 20.0)
-            border = Border(BorderStroke(Color.rgb(0,0,0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.DEFAULT_WIDTHS))
+            border = Border(BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.DEFAULT_WIDTHS))
+
             setOnMouseClicked { event ->
-                val currentPane = this
-                if (event.button == MouseButton.PRIMARY) {
-                    clearPath()
-
-
-                    // Переключение цвета фона между белым и черным
-                    val currentColor = background.fills[0].fill
-
-                    if (currentPane == startPane) {
-                        startPane = null
-                    }
-                    if (currentPane == endPane){
-                        endPane = null
-                    }
-
-                    val newColor = if (currentColor == ColorSigns.UNVISITED.color) ColorSigns.WALL.color else ColorSigns.UNVISITED.color
-                    background = Background(BackgroundFill(newColor, null, null))
-                }
-                if (event.button == MouseButton.SECONDARY) {
-                    clearPath()
-                    val currentColor = background.fills[0].fill
-
-                    var newColor = if (currentColor == ColorSigns.START.color) ColorSigns.EXIT.color  else ColorSigns.START.color
-
-                    if (currentPane == startPane){
-                        updatePaneColor(endPane, ColorSigns.UNVISITED.color)
-                        endPane = startPane
-                        startPane = null
-                    } else if (currentPane == endPane) {
-                        updatePaneColor(startPane, ColorSigns.UNVISITED.color)
-                        startPane = endPane
-                        endPane = null
-                    } else if (startPane != null && endPane != null){
-                        newColor = ColorSigns.UNVISITED.color
-                    } else if (startPane == null){
-                        startPane = currentPane
-                    } else {
-                        endPane = currentPane
-                        newColor = ColorSigns.EXIT.color
-                    }
-
-                    updatePaneColor(currentPane, newColor)
-
-                }
+                handlePaneClick(this, event.button)
             }
         }
     }
 
-    private fun paneCleaning(pane: Pane?){
-        updatePaneColor(pane, ColorSigns.UNVISITED.color)
-        if (pane == startPane){
-            startPane = null
-        } else if (pane == endPane) {
-            endPane = null
+    // Обработчик кликов для панелей
+    private fun handlePaneClick(currentPane: Pane, button: MouseButton) {
+        clearPath()
+
+        when (button) {
+            MouseButton.PRIMARY -> toggleWall(currentPane)
+            MouseButton.SECONDARY -> setStartOrEnd(currentPane)
+            else -> { /* No action needed */ }
         }
     }
 
-    private fun updatePaneColor(pane: Pane?, color: Color){
+    // Логика переключения между стеной и пустым местом
+    private fun toggleWall(currentPane: Pane) {
+        val currentColor = currentPane.background.fills[0].fill
+        if (currentPane == startPane) startPane = null
+        if (currentPane == endPane) endPane = null
+
+        val newColor = if (currentColor == ColorSigns.UNVISITED.color) ColorSigns.WALL.color else ColorSigns.UNVISITED.color
+        updatePaneColor(currentPane, newColor)
+    }
+
+    // Логика установки стартовой и конечной точек
+    private fun setStartOrEnd(currentPane: Pane) {
+        val currentColor = currentPane.background.fills[0].fill
+        var newColor = if (currentColor == ColorSigns.START.color) ColorSigns.EXIT.color else ColorSigns.START.color
+
+        if (currentPane == startPane) {
+            updatePaneColor(endPane, ColorSigns.UNVISITED.color)
+            endPane = startPane
+            startPane = null
+        } else if (currentPane == endPane) {
+            updatePaneColor(startPane, ColorSigns.UNVISITED.color)
+            startPane = endPane
+            endPane = null
+        } else if (startPane != null && endPane != null) {
+            newColor = ColorSigns.UNVISITED.color
+        } else if (startPane == null) {
+            startPane = currentPane
+        } else {
+            endPane = currentPane
+            newColor = ColorSigns.EXIT.color
+        }
+
+        updatePaneColor(currentPane, newColor)
+    }
+
+    // Метод для обновления цвета панели
+    private fun updatePaneColor(pane: Pane?, color: Color) {
         pane?.background = Background(BackgroundFill(color, null, null))
     }
 
+    // Инициализация сетки
     private fun initializeGrid() {
         for (row in 0 until rows) {
             for (col in 0 until cols) {
@@ -160,8 +134,23 @@ class GridController (private var rows: Int = 0, private var cols: Int = 0) {
                 grid.add(pane, col, row)
             }
         }
-
     }
 
+    // Инициализация сетки из лабиринта
+    private fun initializeGridFromMaze(maze: Maze) {
+        for (rowIndex in maze.mazeList.indices) {
+            for (colIndex in maze.mazeList[rowIndex].indices) {
+                val sign = maze.mazeList[rowIndex][colIndex]
+                val paneColor = getPaneColor(sign)
+                val pane = createPane(paneColor)
+                if (paneColor == ColorSigns.START.color) {
+                    startPane = pane
+                } else if (paneColor == ColorSigns.EXIT.color) {
+                    endPane = pane
+                }
 
+                grid.add(pane, colIndex, rowIndex)
+            }
+        }
+    }
 }
